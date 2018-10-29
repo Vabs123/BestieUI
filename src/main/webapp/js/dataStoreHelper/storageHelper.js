89a892271e5eb59ca3d2c544b9c475a5bfa5792b
grover.v <grover.v@media.net>
2018-10-29 16:54:10 +0530

    Dynamic Extension Id extract Done

--- src/main/webapp/js/dataStoreHelper/storageHelper.js
+++ src/main/webapp/js/dataStoreHelper/storageHelper.js
@@ -1,6 +1,8 @@
-var EXTENSION_ID = "odljaphhcggnlafaaonbbnpbofigmelf";
+
 
 function fetchKey( key) {
+    var EXTENSION_ID = document.getElementById("extension_id").innerText;
+    console.log(EXTENSION_ID + "from fetch");
     return new Promise(function(resolve, reject) {
         chrome.runtime.sendMessage(EXTENSION_ID, {fetch: key},
             function (response) {
@@ -14,6 +16,8 @@ function fetchKey( key) {
 }
 
 function updateKey(obj) {
+    var EXTENSION_ID = document.getElementById("extension_id").innerText;
+    console.log(EXTENSION_ID + "from update");
     return new Promise(function(resolve, reject) {
         chrome.runtime.sendMessage(EXTENSION_ID, {update: obj},
             function (response) {
